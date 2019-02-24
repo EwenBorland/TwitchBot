a110a9924853eb47e1b44b5d5ee3e92b246c751c
EwenBorland <32567047+EwenBorland@users.noreply.github.com>
2019-02-24 23:04:59 +0000

    connect to twitch

--- run.py
+++ run.py
@@ -1,11 +1,23 @@
 import configparser
+import socket
 
 #importing irc settings 
 config = configparser.ConfigParser()
 config.read("config.ini")
+
 HOST = config.get('irc','HOST')
 PORT = config.getint('irc','PORT')
 PASS = config.get('irc','PASS')
 NICK = config.get('irc','NICK')
 CHAN = config.get('irc','CHAN')
 
+def ircMessage(message):
+    irc.send(("".join(("PRIVMSG ", CHAN, " :", message, "\r\n"))).encode('utf-8'))
+
+irc = socket.socket()
+irc.connect((HOST,PORT))
+irc.send(("".join(("PASS ", PASS, "\r\n"))).encode('utf-8'))
+irc.send(("".join(("NICK ", NICK, "\r\n"))).encode('utf-8'))
+irc.send(("".join(("JOIN ", CHAN, "\r\n"))).encode('utf-8'))
+
+ircMessage("Bot has connected")
